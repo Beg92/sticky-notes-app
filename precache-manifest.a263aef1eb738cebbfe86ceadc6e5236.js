a8a23806e926e6dc5ba541aa3b8182682f518cd0
Begum V <115528938+Beg92@users.noreply.github.com>
2023-04-20 21:27:29 -0700

    Updates

---
+++ precache-manifest.a263aef1eb738cebbfe86ceadc6e5236.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "bdd9a9e28e13621ab20c",
+    "url": "/sticky-notes-app/static/css/main.158ae4a4.chunk.css"
+  },
+  {
+    "revision": "bdd9a9e28e13621ab20c",
+    "url": "/sticky-notes-app/static/js/main.30779cbb.chunk.js"
+  },
+  {
+    "revision": "c1f5a75c2ca3b682f1b7",
+    "url": "/sticky-notes-app/static/js/runtime~main.11529745.js"
+  },
+  {
+    "revision": "aacd201bf72820a96c02",
+    "url": "/sticky-notes-app/static/js/2.b4876291.chunk.js"
+  },
+  {
+    "revision": "e07c5b2b487ceb44db301d7a018522ca",
+    "url": "/sticky-notes-app/index.html"
+  }
+];
